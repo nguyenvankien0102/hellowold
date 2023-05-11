4e0c859e6e5ec413a6bb1d8f5b1f02f6e3065aa4
nguyenvankien0102 <133183393+nguyenvankien0102@users.noreply.github.com>
2023-05-11 11:33:02 +0700

    Add files via upload

---
+++ bai tap 6.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	float a,b,c,s,diemtb;
+	printf("a:");
+	scanf("%f",&a);
+	
+	printf("b:");
+	scanf("%f",&b);
+	
+	printf("c:");
+	scanf("%f",&c);
+	
+	s=a+b+c;
+	printf("s=%.f\n",s);
+	diemtb=(a+b+c)/3;
+	printf("diemtb=%.f",diemtb);
+	
+	
+}
